f7c1a90cb9ef29eb686aa5721f85f59ab039cac2
agustin <agustin.herranz@gmail.com>
2015-10-27 16:35:15 +0100

    Supress SQLAlchemy warning.

--- autoconstruccion/config.py
+++ autoconstruccion/config.py
@@ -11,6 +11,7 @@ class Development(BaseConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)),
                                                           'instance', 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class Testing(BaseConfig):
