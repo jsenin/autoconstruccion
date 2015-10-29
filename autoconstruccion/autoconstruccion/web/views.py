f8e8bd40347e1de8f37d224749199588d2da106d
agustin <agustin.herranz@gmail.com>
2015-10-29 12:01:01 +0100

    Add views functions. Change landing page template. Refactor names in templates.

--- autoconstruccion/autoconstruccion/web/views.py
+++ autoconstruccion/autoconstruccion/web/views.py
@@ -9,15 +9,32 @@ bp = Blueprint('web', __name__,
                static_url_path='static/web')
 
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/')
 def index():
- 
-    
+    return render_template('index.html')
+
+
+@bp.route('projects', methods=['GET', 'POST'])
+def projects():
     if request.method == 'POST':
         project = Project()
         project.parse(request.form)
         db.session.add(project)
         db.session.commit()
-
     projects = Project.query.all()
-    return render_template('Projects/add.html', projects=projects)
+    return render_template('projects/index.html', projects=projects)
+
+
+@bp.route('projects/<int:project_id>')
+def get_project(project_id):
+    return "Project: {}".format(project_id)
+
+
+@bp.route('users', methods=['GET', 'POST'])
+def users():
+    return "Users"
+
+
+@bp.route('users/<int:user_id>')
+def get_user(user_id):
+    return "user: {}".format(user_id)
