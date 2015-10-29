9028b729a8ad4fe6e031cb6924a1e04e2525b396
agustin <agustin.herranz@gmail.com>
2015-10-29 12:44:28 +0100

    Add dumb user model. Fix .gitignore

--- server_app/autoconstruccion/models.py
+++ server_app/autoconstruccion/models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 
 class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
 
@@ -23,3 +23,19 @@ class Project(db.Model):
         cls.name = data['name']
         cls.description = data['description']
         return cls
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    full_name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    tel = db.Column(db.String(9), nullable=False)
+
+    def __init__(self, full_name, email, tel):
+        self.full_name = full_name
+        self.email = email
+        self.tel = tel
+
+    def __repr__(self):
+        text = "Usuario: \t{}\n\t\temail: {}\n\t\ttelefono: {}"
+        return text.format(self.full_name, self.email, self.tel)
