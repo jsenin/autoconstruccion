78d82f31755ef4ce99fa1c84882747fa1517d74c
agustin <agustin.herranz@gmail.com>
2015-11-11 17:48:08 +0100

    Add new atributes to Project model, also fix the form to support it. Change template to support crsf token. Some PEP8 adjustments

--- server_app/autoconstruccion/forms.py
+++ server_app/autoconstruccion/forms.py
@@ -1,25 +1,22 @@
 from flask_wtf import Form
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, TextAreaField, DateField
 from wtforms import validators
-from wtforms import StringField, TextAreaField, FileField, DateField
 from autoconstruccion.validators import remove_not_numbers
 
 
 class ProjectForm(Form):
 
-    name = StringField('name', [
-        validators.DataRequired(),
-        validators.Length(min=3, max=255)
-    ])
-    description = TextAreaField('description', [validators.Length(min=5)])
-    start_date = DateField('start_date', format='%d/%m/%Y')
-    end_date = DateField('end_date', format='%d/%m/%Y')
-    location = StringField('location', [validators.DataRequired()])
-    phone_number = StringField('phone_number')
-    image = FileField('image')
-
+    name = StringField('name', [validators.DataRequired(), validators.Length(min=3, max=255) ])
+    description = TextAreaField('description', validators=[validators.Length(min=5)])
+    start_date = DateField('start_date', format='%d/%m/%Y', validators=[validators.DataRequired()])
+    end_date = DateField('end_date', format='%d/%m/%Y', validators=[validators.DataRequired()])
+    location = StringField('location', validators=[validators.DataRequired()])
+    contact_phone = StringField('phone_number', validators=[validators.DataRequired()])
+    image = FileField('image', validators=[FileAllowed(['jrp', 'png'], 'Only jpg or png images please.')])
 
 class UserForm(Form):
-    full_name = StringField('Full name', [ validators.DataRequired(), validators.Length(min=3, max=255)],
+    full_name = StringField('Full name', [validators.DataRequired(), validators.Length(min=3, max=255)],
                             description='Your full name')
     email = StringField('Email', [validators.DataRequired(), validators.email('Email not valid')])
     phone_number = StringField('Phone number', filters=(remove_not_numbers,))
--- server_app/autoconstruccion/models.py
+++ server_app/autoconstruccion/models.py
@@ -7,31 +7,18 @@ class Project(db.Model):
     __tablename__ = 'projects'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     start_date = db.Column(db.Date, nullable=False)
     end_date = db.Column(db.Date, nullable=False)
-    image = db.Column(db.BLOB, nullable=False)
+    image = db.Column(db.BLOB)
     location = db.Column(db.String(200), nullable=False)
     contact_phone = db.Column(db.String(15), nullable=False)
     manager_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     manager = db.relationship('User', uselist=False)
 
-    def __init__(self, name=None, description=None):
-        self.parse({'name': name, 'description': description})
-
     def __repr__(self):
         return "Project: {} \nDescription: {}".format(self.name, self.description)
-
-    def parse(self, data):
-        self.name = data['name']
-        self.description = data['description']
-
-    @classmethod
-    def from_dict(cls, data):
-        cls.name = data['name']
-        cls.description = data['description']
-        return cls
 
 
 class User(db.Model):
