551f0bff15302b5ba577ff9e478578387e9c988d
agustin <agustin.herranz@gmail.com>
2015-11-12 15:50:22 +0100

    Add setup.py to autoconstruccion package

---
+++ server_app/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'Flask==0.10.1',
+    'Flask-SQLAlchemy==2.1',
+    'Flask-WTF==0.12',
+    'SQLAlchemy==1.0.9',
+]
+
+
+setup(
+    name='autoconstruccion',
+    version='0.0.0.dev0',
+    package_dir={'': 'autoconstruccion'},
+    packages=find_packages('autoconstruccion'),
+    url='https://github.com/autoconstruccion/autoconstruccion',
+    author='Creepy Coconuts',
+    author_email='',
+    description='Autoconstruccion web app',
+    install_requires=requires,
+    zip_safe=False,
+)
