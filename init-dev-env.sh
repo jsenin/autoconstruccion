11696cf7a96ea5e75de2a7c1a5ac1b5bc478c74f
jorge <jorge@senin.org>
2015-11-20 13:06:07 +0100

    better install process

--- init-dev-env.sh
+++ init-dev-env.sh
@@ -1,63 +1,78 @@
-#!/bin/bash
+#!/bin/bash 
 
-app='autoconstruccion'
+APP_DIR='server_app'
+PYTHON=$(which python3)
+PYTHON_MAYOR_REQUIRED=3
+PYTHON_MINOR_REQUIRED=4
 
-python="python"
-major=$($python  -c 'import sys; print(sys.version_info[0])')
 
-if [ $major -eq 2 ]
-then
-    python='python3'
-    major=$($python  -c 'import sys; print(sys.version_info[0])')
-    if [ $major -ge 3 ]
-    then echo
-    else
-        echo "Python 3 it's mandatory"
-        exit
-    fi
-fi
+function require_python_mayor_minor_version_installed {
 
-minor=$($python -c 'import sys; print(sys.version_info[1])')
-if [ $minor -lt 4 ]
-    then
-        echo "Minimun Python version supported it's 3.4"
-        exit
-fi
+    major=$(${PYTHON} -c 'import sys; print(sys.version_info[0])');
+    minor=$(${PYTHON} -c 'import sys; print(sys.version_info[1])');
 
-echo "Using Python:" $python_version
-echo $($python -V)
+    if ! ( [[ ${PYTHON_MAYOR_REQUIRED} == $major ]] && [[ ${PYTHON_MINOR_REQUIRED} == $minor ]] ); then
+        install_error "Python ${PYTHON_MAYOR_REQUIRED}.${PYTHON_MINOR_REQUIRED} is required";
+    fi;
 
+}
+
+function install_error {
+    echo $1 
+    exit 1;
+}
+
+function install_venv_and_pip_at_debian_as_workaround {
+        echo "module venv fails, maybe this is beacause debiand and ubuntu has a pip issue "
+        echo "http://askubuntu.com/questions/488529/pyvenv-3-4-error-returned-non-zero-exit-status-1"
+        echo "trying to install with a workaround"
+        ${PYTHON} -m venv --without-pip venv
+        source venv/bin/activate
+        curl https://bootstrap.pypa.io/get-pip.py | python
+        deactivate
+
+}
 # create virtual environment
-mkdir venv
-$python -m venv ./venv
+function install_venv  {
+    ${PYTHON} -m venv ./venv 1&> /dev/null
 
-if [[ $? != 0 ]]; then
-	echo "module venv fails, maybe this is beacause debiand and ubuntu has a pip issue "
-	echo "http://askubuntu.com/questions/488529/pyvenv-3-4-error-returned-non-zero-exit-status-1"
-	echo "trying to install with a workaround"
-	$python -m venv --without-pip ./venv
-	source venvdir/bin/activate
-	curl https://bootstrap.pypa.io/get-pip.py | python
-	deactivate
+    if [[ $? != 0 ]]; then
+        install_venv_and_pip_at_debian_as_workaround
+    fi; 
 
-fi ;
+    if [ ! -e ./venv/bin/activate ]; then
+        install_error "venv not installed";
+    fi;
 
-source ./venv/bin/activate
+}
 
-pip="$python -m pip"
-if $( cat $pip | grep-i 'No module' ); then
-	echo "necesitas instalar pip"
-fi ;
- 
-echo $($pip -V)
+function require_python_pip {
+    PIP="${PYTHON} -m pip"
+    if $( ${PIP} | grep -i 'No module' ); then
+        install_error 'PIP module not present.'
+    fi ;
 
-$pip install -r requirements-dev.txt
-$pip install -r requirements.txt
+}
 
-#mkdir ./$app
+function install_requirements {
+
+    source ./venv/bin/activate
+
+    PIP=$(which pip);
+    ${PIP} install -r requirements-dev.txt 
+    ${PIP} install -r requirements.txt
+
+    deactivate
+}
+
+require_python_mayor_minor_version_installed ${PYTHON_MAYOR_REQUIRED} ${PYTHON_MINOR_REQUIRED}
+install_venv
+install_requirements
 
 # make development config file
-mkdir ./$app/instance
-touch ./$app/instance/config.py
 
+mkdir -p ./${APP_DIR}/instance 
+touch ./${APP_DIR}/instance/config.py
 
+source ./venv/bin/activate
+python server_app/create_db.py
