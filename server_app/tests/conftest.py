56e1249a7b4062dd58460a5697d315db8961b344
agustin <agustin.herranz@gmail.com>
2015-11-24 10:12:39 +0100

    Move app fixture to global conftest file. Add teardown with drop_db. Remove failing test from test_views unittest style, already in test_integration

---
+++ server_app/tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from autoconstruccion import create_app, db
+
+
+# define app fixture for pytest-flask
+@pytest.fixture()
+def app(request):
+    # for now we test over sqlite in memory, so no empty db needed
+    test_app = create_app('TESTING_MEMORY')
+    db.init_app(test_app)
+    with test_app.test_request_context():
+        # test that the database on we are testing it's ended with '_test'
+        # to avoid data corruption and/or deletion
+        db_uri = test_app.config['SQLALCHEMY_DATABASE_URI']
+        if not db_uri.endswith('_test'):
+            pytest.skip(msg="Testing over a none '_test' database. Skipping")
+        db.create_all()
+
+    def teardown():
+        with test_app.test_request_context():
+            db.drop_all()
+    request.addfinalizer(teardown)
+
+    return test_app
