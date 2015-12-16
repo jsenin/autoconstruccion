f6d1f7387669010eba39346c3f68e90ee5712ebd
Sergio <sergio@tinkerista.com>
2015-12-16 12:42:16 +0100

    arreglados comentarios pull request

--- server_app/autoconstruccion/admin/views.py
+++ server_app/autoconstruccion/admin/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template
-from autoconstruccion.models import Project, db, Event, User
-#Esto lo he cambiado yo(sergio espero no haber metido la pata) 
+from autoconstruccion.models import Project
 
 bp = Blueprint('admin', __name__,
                template_folder='templates',
