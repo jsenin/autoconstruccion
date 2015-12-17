f439d6d4fdc843ed7956ff3927e85a09d93d9ff6
agustin <agustin.herranz@gmail.com>
2015-12-17 13:55:21 +0100

    Remove unecesary jinja test. Fix test.

--- server_app/autoconstruccion/helpers/helpers.py
+++ server_app/autoconstruccion/helpers/helpers.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, current_app
 from flask import make_response, render_template
 
-
 bp = Blueprint('helpers', __name__)
 log = current_app.logger
 
@@ -15,10 +14,3 @@ def error_page_not_found(exception):
 def error_internal_server_error(exception):
     log.error(exception)
     return make_response(render_template('error/err_500.html'), 500)
-
-
-@bp.add_app_template_test
-def startswith(string, prefix):
-    if not issubclass(string, str):
-        return False
-    return string.startswith(prefix)
